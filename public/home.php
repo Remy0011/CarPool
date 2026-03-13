3384e564c47b5d72b38b4c03efb9b585f1146565
Remy0011 <remy.deiber1999@gmail.com>
2026-03-13 16:41:41 +0100

    try to add connexion page

--- public/home.php
+++ public/home.php
@@ -1,7 +1,6 @@
 <body>
 <div class="navbar">
     <h1>Mon Application</h1>
-    <a href="?logout=1">Déconnexion</a>
 </div>
 
 <div class="container">
@@ -11,9 +10,6 @@
 
         <div class="user-info">
             <h3>Informations utilisateur :</h3>
-            <p><strong>Nom :</strong> <?php echo htmlspecialchars($_SESSION['user_name']); ?></p>
-            <p><strong>Email :</strong> <?php echo htmlspecialchars($_SESSION['user_email']); ?></p>
-            <p><strong>ID :</strong> <?php echo htmlspecialchars($_SESSION['user_id']); ?></p>
         </div>
     </div>
 </div>
